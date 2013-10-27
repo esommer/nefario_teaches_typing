0d43b8347838544b6039ef18ec57c9eb436394af
Mary Rose Cook <maryrosecook@maryrosecook.com>
2013-10-27 12:57:57 -0400

    rm Key and just do everything in Keyboard
    * Also explicitly make fn that is all about view, rather than mixing logic and view.
    * Use passed in document to create key elements.

--- assets/js/Keyboard.js
+++ assets/js/Keyboard.js
@@ -1,29 +1,24 @@
 ;(function(exports) {
-  function Keyboard (element) {
-	  this.element = element;
-	  this.build();
+  var qwertydict = {
+	  0 : "~", 1 : "1", 2 : "2", "27":"esc", 65:"a", 66:"b", 67:"c", 68:"d",
+    69:"e", 70:"f", 71:"g", 72:"h", 73:"i", 74:"j", 75:"k", 76:"l", 77:"m",
+    78:"n", 79:"o", 80:"p", 81:"q", 82:"r", 83:"s", 84:"t", 85:"u", 86:"v",
+    87:"w", 88:"x", 89:"y", 90:"z", 186:";"
+  };
+
+  function Keyboard (document, element) {
+    this.display(document, element);
   }
 
-  Keyboard.prototype.build = function () {
-    var qwertydict = {
-	    0 : "~", 1 : "1", 2 : "2", "27":"esc", 65:"a", 66:"b", 67:"c", 68:"d",
-      69:"e", 70:"f", 71:"g", 72:"h", 73:"i", 74:"j", 75:"k", 76:"l", 77:"m",
-      78:"n", 79:"o", 80:"p", 81:"q", 82:"r", 83:"s", 84:"t", 85:"u", 86:"v",
-      87:"w", 88:"x", 89:"y", 90:"z", 186:";"
-    };
-
+  Keyboard.prototype.display = function (document, element) {
 	  for (var num in qwertydict) {
-		  var keySquare = new Key(qwertydict[num]);
-		  this.element.appendChild(keySquare.contents);
+      var letter = qwertydict[num];
+	    var contents = document.createElement("li");
+	    contents.setAttribute("class","letter key_" + letter);
+	    contents.innerHTML = letter;
+		  element.appendChild(contents);
 	  }
-  }
-
-  function Key (letter) {
-	  this.letter = letter;
-	  this.contents = document.createElement("li");
-	  this.contents.setAttribute("class","letter key_"+this.letter);
-	  this.contents.innerHTML = this.letter;
-  }
+  };
 
   exports.Keyboard = Keyboard;
 })(this);
