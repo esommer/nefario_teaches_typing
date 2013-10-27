7eefe9aeb0d1cd22e6c7941e612f9add6039b0f4
Mary Rose Cook <maryrosecook@maryrosecook.com>
2013-10-27 12:22:11 -0400

    No need for QWERTYDICT to be global or in main file

--- assets/js/Keyboard.js
+++ assets/js/Keyboard.js
@@ -5,8 +5,15 @@
   }
 
   Keyboard.prototype.build = function () {
-	  for (var num in QWERTYDICT) {
-		  var keySquare = new Key(QWERTYDICT[num]);
+    var qwertydict = {
+	    0 : "~", 1 : "1", 2 : "2", "27":"esc", 65:"a", 66:"b", 67:"c", 68:"d",
+      69:"e", 70:"f", 71:"g", 72:"h", 73:"i", 74:"j", 75:"k", 76:"l", 77:"m",
+      78:"n", 79:"o", 80:"p", 81:"q", 82:"r", 83:"s", 84:"t", 85:"u", 86:"v",
+      87:"w", 88:"x", 89:"y", 90:"z", 186:";"
+    };
+
+	  for (var num in qwertydict) {
+		  var keySquare = new Key(qwertydict[num]);
 		  this.element.appendChild(keySquare.contents);
 	  }
   }
